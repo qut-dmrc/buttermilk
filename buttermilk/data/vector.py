fe741493b59fa8401bd275239fb506ab34af4437
Nicolas Suzor <nic@suzor.com>
2025-01-14 20:59:53 +1000

    not quite working but nearly

--- buttermilk/data/vector.py
+++ buttermilk/data/vector.py
@@ -1,8 +1,10 @@
 import os
 import argparse
 
-from typing import Optional, Sequence
+from typing import Optional, Self, Sequence
+import numpy as np
 import pandas as pd
+import pydantic
 from tqdm import tqdm
 
 import chromadb
@@ -11,6 +13,9 @@ import google.generativeai as genai
 from google.cloud import aiplatform
 from chromadb.utils import embedding_functions
 
+from chromadb.utils.embedding_functions.chroma_langchain_embedding_function import (  # noqa: F401
+    create_langchain_embedding,
+)
 from buttermilk._core.runner_types import RecordInfo
 from buttermilk.utils.save import upload_dataframe_json
 from buttermilk.utils.utils import read_text
@@ -28,7 +33,7 @@ from langchain_chroma import Chroma
 from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
 
 from typing import Any, Mapping, Optional, Sequence
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PrivateAttr
 from chromadb import Documents, Embeddings
 from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
 
@@ -56,28 +61,62 @@ class ChunkedDocument(BaseModel):
 class GoogleVertexEmbeddings(BaseModel):
     embedding_model: str = MODEL_NAME
     task: str = "RETRIEVAL_DOCUMENT"
+    collection_name: str
     dimensionality: Optional[int] = None
     chunk_size: int = 4000
     chunk_overlap:int = 1000
+    persist_directory: str = ".chroma"
+    
+    _collection = PrivateAttr(default=None)
+    _embedding_model = PrivateAttr(default=None)
+    _client = PrivateAttr(default=None)
+    _lc_embedding_function = PrivateAttr(default=None)
 
-    def get_embeddings(self, chunked_documents: Sequence[ChunkedDocument]) -> Sequence[ChunkedDocument]:
-        model = TextEmbeddingModel.from_pretrained(self.embedding_model)
-        kwargs = dict(output_dimensionality=self.dimensionality, auto_truncate=False)
-        
+    @pydantic.model_validator(mode="after")
+    def load_models(self) -> Self:
+        self._embedding_model = TextEmbeddingModel.from_pretrained(self.embedding_model)
+
+        # Instantiate a persistent chroma client in the persist_directory.
+        self._client = chromadb.PersistentClient(path=self.persist_directory)
+
+        self._lc_embedding_function = create_langchain_embedding(self)
+
+        return self
+
+    @property
+    def get_collection(self) -> Any:
+        self._collection = self._client.get_collection(self.collection_name, embedding_function=self._lc_embedding_function)
+
+    @property
+    def db(self) -> Any:
+        db = Chroma(self.collection_name, persist_directory=self.persist_directory, embedding_function=self._lc_embedding_function, create_collection_if_not_exists=False)
+        return db
+    
+    def embed_records(self, chunked_documents: Sequence[ChunkedDocument]) -> Embeddings:
         inputs = [TextEmbeddingInput(text=chunk.chunk_text, task_type=self.task, title=chunk.chunk_title) for chunk in chunked_documents]
-        
+        return self._embed(inputs)
+
+    def embed_documents(self, texts: Documents) -> Embeddings:
+        inputs = [TextEmbeddingInput(text=text, task_type=self.task) for text in texts]
+        return self._embed(inputs)
+
+    def embed_query(self, query: str) -> Any:
+        inputs = [TextEmbeddingInput(text=query, task_type=self.task)]
+        return self._embed(inputs)
+
+    def _embed(self, inputs: Sequence[TextEmbeddingInput]) -> Embeddings:
+        kwargs = dict(output_dimensionality=self.dimensionality, auto_truncate=False)
         embeddings = []
-        for chunk in inputs:
+        for chunk in inputs:        
             # we could optimise by passing multiple chunks as a time, but we'd have to calculate the max input token to do that
-            e = model.get_embeddings([chunk], **kwargs)
+            e = self._embedding_model.get_embeddings([chunk], **kwargs)
             embeddings.extend(e)
-        
-        for i, e in enumerate(embeddings):
-            chunked_documents[i].embedding = e.values
-            
-        return chunked_documents
-    
-    
+
+        embeddings = [ embedding.values  for embedding in embeddings]
+
+        return embeddings
+
+
     def prepare_docs(self, records: Sequence[RecordInfo]) -> Sequence[ChunkedDocument]:
         # Chunking
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
@@ -92,35 +131,35 @@ class GoogleVertexEmbeddings(BaseModel):
         return chunked_documents
 
 
-    def make_vectorstore_chromadb(self,
-        collection_name: str,
-        records: Sequence[RecordInfo],
-        embeddings_path: str,
-        persist_directory: str = ".chroma",
-    ):
-        records = pd.read_json(dataset, orient='records', lines=True)
-
-        chunks = self.prepare_docs(records=records)
+    def get_embedded_records(self, chunked_documents: Sequence[ChunkedDocument]) -> Sequence[ChunkedDocument]:
         
-        embeddings = self.get_embeddings(chunks)
-
-        df = pd.DataFrame(embeddings)
-        upload_dataframe_json(data=df, uri=embeddings_path)
-
-        # Instantiate a persistent chroma client in the persist_directory.
-        client = chromadb.PersistentClient(path=persist_directory)
-
-        collection = client.create_collection(name=collection_name)
+        embeddings = self.embed_records(chunked_documents)
         
-        # Add data to ChromaDB
-        for chunk in embeddings:
-            metadata = dict(**chunk.structured_data)
-            metadata.update({"chunk_index": chunk.chunk_index, "chunk_title": chunk.chunk_title })
-            collection.add(
-                ids=[chunk.chunk_id],
-                embeddings=[chunk.embedding],
-                metadatas=[metadata], 
-                documents=[chunk.chunk_text],
+        for i, embedding in enumerate(embeddings):
+            chunked_documents[i].embedding = embedding
+            
+        return chunked_documents
+    
+
+    def create_vectorstore_chromadb(self,
+                                  records: list[RecordInfo], 
+                                  save_path: str,
+    ) -> pd.DataFrame:
+        
+        docs = self.prepare_docs(records=records)
+        embedded_records = self.get_embedded_records(docs)
+        df_embeddings = pd.DataFrame.from_records([x.model_dump() for x in embedded_records])
+        df_embeddings.to_json(save_path, orient='records', lines=True)
+
+        ids = df_embeddings['chunk_id'].to_list()
+        embeddings = df_embeddings['embedding'].to_list()
+        metadata = df_embeddings[['record_id','document_title','chunk_index']].to_dict(orient='records')
+
+        # Add all the embeddings we have currently
+        self._collection.add(
+                ids=ids,
+                embeddings=embeddings,
+                metadatas=metadata,
             )
-        return client, collection
 
+        return df_embeddings
