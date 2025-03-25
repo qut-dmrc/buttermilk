8422693eb5b8184f1fba1a9c39e663d1507b1e43
Nicolas Suzor <nic@suzor.com>
2025-03-25 14:09:14 +1000

    converted to agent AND tool...

--- buttermilk/tools/fetch.py
+++ buttermilk/tools/fetch.py
@@ -5,14 +5,15 @@ from typing import Any, Self
 import pydantic
 from pydantic import PrivateAttr
 
-from buttermilk._core.agent import ToolConfig
+from buttermilk._core.agent import Agent, ToolConfig
+from buttermilk._core.contract import AgentInput, AgentOutput
 from buttermilk._core.runner_types import Record
 from buttermilk.runner.helpers import prepare_step_df
 from buttermilk.utils.media import download_and_convert
 from buttermilk.utils.utils import extract_url
 
 
-class Fetch(ToolConfig):
+class Fetch(Agent, ToolConfig):
     _data: dict[str, Any] = PrivateAttr(default={})
     _data_task: asyncio.Task = PrivateAttr()
 
@@ -21,26 +22,26 @@ class Fetch(ToolConfig):
         self._data_task = asyncio.create_task(prepare_step_df(self.data_cfg))
         return self
 
-    async def _run(self, record_id: str) -> Record | None:
-        while not self._data_task.done():
-            await asyncio.sleep(1)
+    async def _process(self, input_data: AgentInput, **kwargs) -> AgentOutput | None:
+        """Entry point when running this as an agent."""
+        prompt = input_data.inputs["prompt"]
+        if uri := extract_url(prompt):
+            record = await self._run(uri=uri)
+        else:
+            record = await self._run(record_id=prompt)
+        return AgentOutput(
+            agent_id=self.id,
+            agent_name=self.name,
+            content=record.fulltext(),
+            records=[record],
+        )
 
-        for dataset in self._data.values():
-            rec = dataset.query("record_id==@record_id")
-            if rec.shape[0] == 1:
-                return Record(**rec.iloc[0].to_dict())
-            if rec.shape[0] > 1:
-                raise ValueError(
-                    f"More than one record found for query record_id == {record_id}",
-                )
-
-        return None
-
-    async def get_record(
+    async def _run(
         self,
         record_id: str | None = None,
         uri: str | None = None,
     ) -> Record | None:
+        """Entry point when running as a tool."""
         assert (record_id or uri) and not (record_id and uri), (
             "You must provide EITHER record_id OR uri."
         )
@@ -53,4 +54,19 @@ class Fetch(ToolConfig):
             uri = extract_url(uri)
             record = download_and_convert(uri=uri)
             return record
+        return None
+
+    async def get_record_dataset(self, record_id: str) -> Record | None:
+        while not self._data_task.done():
+            await asyncio.sleep(1)
+
+        for dataset in self._data.values():
+            rec = dataset.query("record_id==@record_id")
+            if rec.shape[0] == 1:
+                return Record(**rec.iloc[0].to_dict())
+            if rec.shape[0] > 1:
+                raise ValueError(
+                    f"More than one record found for query record_id == {record_id}",
+                )
+
         return None
