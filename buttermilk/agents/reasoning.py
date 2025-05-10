00fdf4dcca26bb8a46910e70a211e4d428877356
Nicolas Suzor <nic@suzor.com>
2025-05-10 11:17:21 +1000

    ui config

--- buttermilk/agents/reasoning.py
+++ buttermilk/agents/reasoning.py
@@ -8,26 +8,39 @@ from pydantic import BaseModel, Field
 # Buttermilk core imports
 from buttermilk.agents.llm import LLMAgent  # Base class for LLM-powered agents
 
-class Agreement(BaseModel):
+
+class Expert(BaseModel):
+    """Expert class representing an expert's name and answer."""
+
+    name: str
+    answer_id: str
+
+
+class Divergence(BaseModel):
     topic: str = Field(..., description="The key topic, point, or fact at hand.")
-    divergence: str = Field(..., description="The specific divergence or difference in opinion.")
-    agreement: str = Field(..., description="The specific agreement or common ground.")
+    positions: list[dict[list[Expert], str]] = Field(..., description="Summary of positions held by each group of experts (only include material differences).")
 
-    
 
-class Reasons(BaseModel):
+class Differences(BaseModel):
     conclusion: str = Field(..., description="Your conclusion or final answer summarizing current tentative conclusions and outlining any uncertainty or divergence.")
-    reasons: list[str] = Field(..., description="Each element should represent a single step in logical reasoning.")
+    divergences: list[Divergence] = Field(..., description="A list of divergences, where each divergence represents a disagreement or difference in opinion among experts.")
 
     def __str__(self):
         """Returns a nicely formatted MarkDown representation of the evaluation."""
-        reasons_str = "\n\n\t".join(f"- {reason}" for reason in self.reasons)
+        divergences_str = "\n\n".join(
+            f"### Divergence: {divergence.topic}\n"
+            + "\n".join(
+                f"\t- {position} ({', '.join([f'{expert.name} ({expert.answer_id})' for expert in group])})"
+                for group, position in divergence.positions
+            )
+            for divergence in self.divergences
+        )
         return (
-            f"**Reasoning:**\n\n{reasons_str}"
+            f"## Conclusion:\n {self.conclusion}\n{divergences_str}"
         )
 
 
 class Differentiator(LLMAgent):
     """Analysis of differences and similarities between answers."""
 
-    _output_model: type[BaseModel] | None = Reasons
+    _output_model: type[BaseModel] | None = Differences
