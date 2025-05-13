0e8d0102b88d4c696caf2f6d51a53c7559f7379d
Nicolas Suzor <nic@suzor.com>
2025-05-13 16:39:11 +1000

    dont auto load ragzot

--- buttermilk/tools/__init__.py
+++
@@ -1,6 +0,0 @@
-from buttermilk.agents.fetch import FetchRecord
-from buttermilk.tools.characters import CharacterGenerator
-from buttermilk.tools.rag.ragzot import RagZot
-
-ANY = ["FetchRecord", "CharacterGenerator", "ragzot"]
-AVAILABLE_TOOLS = {"fetch_record": FetchRecord, "character_generator": CharacterGenerator, "ragzot": RagZot}
